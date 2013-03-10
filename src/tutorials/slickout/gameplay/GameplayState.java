8c69fc03c5c8626313a5814d637017bcb28f0a8c
davechang92 <d_solidsnake@hotmail.com>
2013-03-10 21:00:28 +0000

    changed level screen and got rid of gameplay background

--- src/tutorials/slickout/gameplay/GameplayState.java
+++ src/tutorials/slickout/gameplay/GameplayState.java
@@ -154,7 +154,7 @@ public class GameplayState extends BasicGameState {
 			throws SlickException {
  
 		// Background
-		level.getBackground().render(gr);
+		//level.getBackground().render(gr);
  
 		// Bumpers
 		level.getLeftBumper().render(gr);
@@ -193,14 +193,14 @@ public class GameplayState extends BasicGameState {
 			gr.drawString(message, 300, 300);
 		}
 		
-		//draws sensor values at bottom of screen
+		/*draws sensor values at bottom of screen
 		gr.drawString("Power Ups Collected: "+ powerUpCollectionSensor.getValue() + " / " + powerUpProductionSensor.getValue(), 50,670);
 		gr.drawString("Lives Lost: "+ livesLostSensor.getValue(), 50, 700);
 		gr.drawString("Bricks hit: "+ bricksHitSensor.getValue(), 50, 730);
 		gr.drawString("Paddle hit: "+ paddleHitSensor.getValue(), 450, 670);
 		gr.drawString("PowerUp P: "+ level.getPowerUpP(), 450, 700);
 		gr.drawString("ExtraRed P: "+ level.getExtraRedP(), 450, 730);
-		gr.drawString("ExtraYellow P: "+ level.getExtraYellowP(), 450, 760);
+		gr.drawString("ExtraYellow P: "+ level.getExtraYellowP(), 450, 760);*/
 
 		//long currentTime = System.currentTimeMillis();
 		//gr.drawString("Time since start: " + (Math.round((currentTime - startTime)/100)), 450, 730);
--- src/tutorials/slickout/mainmenu/BriefingGameState.java
+++ src/tutorials/slickout/mainmenu/BriefingGameState.java
@@ -1,5 +1,5 @@
 package tutorials.slickout.mainmenu;
- 
+
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
 import org.newdawn.slick.Image;
@@ -7,23 +7,20 @@ import org.newdawn.slick.MouseListener;
 import org.newdawn.slick.SlickException;
 import org.newdawn.slick.state.BasicGameState;
 import org.newdawn.slick.state.StateBasedGame;
- 
+
 import tutorials.slickout.GameInfo;
-import tutorials.slickout.gameplay.GameplayState;
-import tutorials.slickout.playerinfo.PlayerInfo;
- 
-public class MainMenuGameState extends BasicGameState implements MouseListener{
- 
+
+public class BriefingGameState extends BasicGameState implements MouseListener {
+
 	private Image background;
 	private Image selector;
-	//1 = start game; 2 = instructions; 3 = high scores; 4 = quit; -1 = none;
 	private int selection; 
 	private int optionSelected;
 	private int topScore;
  
 	@Override
 	public int getID() {
-		return 0;
+		return 3;
 	}
  
 	@Override
@@ -34,16 +31,13 @@ public class MainMenuGameState extends BasicGameState implements MouseListener{
 		
 		selection = -1;
 		optionSelected = selection;
- 
-		if ( GameInfo.getCurrentGameInfo() != null){
-			topScore = ( topScore > GameInfo.getCurrentGameInfo().getPlayerInfo().getScore() ) ? topScore : GameInfo.getCurrentGameInfo().getPlayerInfo().getScore(); 
-		}
+
 	}
  
 	@Override
 	public void init(GameContainer container, StateBasedGame game)
 			throws SlickException {
-		background = new Image("data/dynamoMenuBig.jpg");
+		background = new Image("data/mainmenu.jpg");
 		selector = new Image("data/selector.png");
 	}
  
@@ -53,14 +47,13 @@ public class MainMenuGameState extends BasicGameState implements MouseListener{
 		background.draw();
  
 		if(selection == 1){
-			selector.draw(400, 355);
+			selector.draw(158, 310);
+			selector.draw(694, 310);
+ 
 			GameInfo.createNewGameInfo();
 		}else if(selection == 2){
-			selector.draw(400, 405);
-		}else if(selection == 3){
-			selector.draw(400, 455);
-		}else if(selection == 4){
-			selector.draw(400, 505);
+			selector.draw(158, 474);
+			selector.draw(694, 474);
 		}
  
 		g.drawString("TOPSCORE : " + topScore, 10, 10) ;
@@ -70,37 +63,28 @@ public class MainMenuGameState extends BasicGameState implements MouseListener{
 	public void update(GameContainer container, StateBasedGame game, int delta)
 			throws SlickException {
 		if(optionSelected == 1){
-			String levelfile = "data/level1.lvl";
-			// obtain the game state
-			GameplayState gameplay = (GameplayState) game.getState(1);
-
-			gameplay.setLevelFile(levelfile);
-
-			game.enterState(1);
-		}else if(optionSelected == 4){
+			// TODO log
+			game.enterState(2);
+		}else if(optionSelected == 2){
 			System.exit(0);
 		}
 	}
  
 	public void mouseMoved(int oldx, int oldy, int newX, int newY){
  
-		selection = -1;
-
-		if(newX > 479 && newX < 777){
-			if ( newY > 369 && newY < 423){
+		if(newX > 228 && newX < 702){
+			if ( newY > 308 && newY < 389){
 				selection = 1;
-			}else if ( newY > 423 && newY < 477){
+			}else if ( newY > 475 && newY < 544){
 				selection = 2;
-			}else if ( newY > 477 && newY < 531){
-				selection = 3;
-			}else if ( newY > 531 && newY < 585){
-				selection = 4;
+			}else {
+				selection = -1;
 			}
 		}
-		
 	}
  
 	public void mouseClicked(int button, int x, int y, int clickCount){
 		optionSelected = selection;
 	}
+
 }
